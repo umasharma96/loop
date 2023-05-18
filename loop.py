8e10ddbb0cb5af1b15a4be123aec61c34597a0a8
uma <uma22@navgurukul.org>
2023-05-18 08:02:59 +0530

    loop question

--- loop.py
+++ loop.py
@@ -1,6 +1,46 @@
-n=input("enter")
-i=0
-while i<10:
+# n=input("enter")
+# i=0
+# while i<10:
+#       i+=1
+#       print(i)
+
+
+# n=int(input("enter"))
+# i=1
+# sum=0
+# while  i<=n:
+#        sum=sum+i
+#        i+=1
+#        print(sum)
+
+# n=int(input("enter"))
+# i=2
+# sum=0
+# while i<=n*2:
+#        sum=i+sum
+#        i+=2
+#        print(sum)
+
+# n=int(input("enter"))
+# i=2
+# sum=0
+# while   sum<1000:
+#          sum+=i
+#          print(i)
+#          i+=2
+# else:
+#         print(sum)
+
+# n=int(input("enter"))
+# i=0
+# while  i<100:
+#        i+=7
+#        print(i)
+
+n=int(input("enter"))
+i=1
+p=1
+while i<=n:
+      p*i
       i+=1
-      print(i)
-      
+      print(p)
